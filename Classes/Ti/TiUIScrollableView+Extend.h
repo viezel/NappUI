80ee721c43105c43d961d4c811fffba1c07d7137
Mads Møller <mm@napp.dk>
2013-06-22 21:31:50 +0200

    scrollable view

---
+++ Classes/Ti/TiUIScrollableView+Extend.h
@@ -0,0 +1,12 @@
+/**
+ * Module developed by Napp
+ * Author Mads Møller
+ * www.napp.dk
+ */
+
+#import "TiProxy.h"
+#import "TiUIScrollableView.h"
+
+@interface TiUIScrollableView (TiUIScrollableView_Extend)
+
+@end
---
+++ Classes/Ti/TiUIScrollableView+Extend.m
@@ -0,0 +1,45 @@
+/**
+ * Module developed by Napp
+ * Author Mads Møller
+ * www.napp.dk
+ */
+
+#import "TiUIScrollableView+Extend.h"
+
+@implementation TiUIScrollableView (TiUIScrollableView_Extend)
+
+-(void)setPagingControlCurrentIndicatorColor_:(id)args
+{
+    TiColor *val = [TiUtils colorValue:args];
+    
+    if (val != nil)
+    {
+        UIPageControl *pg = [self pagecontrol];
+        [pg setCurrentPageIndicatorTintColor:[[val _color] retain]];
+        pg.currentPageIndicatorTintColor = [[val _color] retain];
+
+        if (showPageControl && (scrollview != nil) && ([[scrollview subviews] count] > 0))
+        {
+            [self refreshScrollView:[self bounds] readd:NO];
+        }
+    }
+}
+
+-(void)setPagingControlIndicatorColor_:(id)args
+{
+    TiColor *val = [TiUtils colorValue:args];
+
+    if (val != nil)
+    {
+        UIPageControl *pg = [self pagecontrol];
+        [pg setPageIndicatorTintColor:[[val _color] retain]];
+        pg.pageIndicatorTintColor = [[val _color] retain];
+
+        if (showPageControl && (scrollview != nil) && ([[scrollview subviews] count] > 0))
+        {
+            [self refreshScrollView:[self bounds] readd:NO];
+        }
+    }
+}
+
+@end
