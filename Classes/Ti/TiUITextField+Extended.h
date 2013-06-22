4fda306cf121cea0dec1b132ebc0a89deec1c893
Mads Møller <mm@napp.dk>
2013-06-22 21:33:36 +0200

    textfield hintcolor

---
+++ Classes/Ti/TiUITextField+Extended.h
@@ -0,0 +1,12 @@
+/**
+ * Module developed by Napp
+ * Author Mads Møller
+ * www.napp.dk
+ */
+
+#import "TiBase.h"
+#import "TiUITextField.h"
+#import <UIKit/UIKit.h>
+
+@interface TiUITextField (Extended)
+@end
