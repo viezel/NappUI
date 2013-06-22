1cc5aca750d5870a07232da8fb3dc6c7f8198357
Mads Møller <mm@napp.dk>
2013-06-22 21:33:23 +0200

    tabgroup

---
+++ Classes/Ti/TiUITabGroup+Extend.h
@@ -0,0 +1,19 @@
+/**
+ * Module developed by Napp
+ * Author Mads Møller
+ * www.napp.dk
+ *
+ * by ryugoo
+ */
+
+#import "TiBase.h"
+#import "TiUITabGroup.h"
+
+@interface TiUITabGroup (TiUITabGroup_Extend)
+
+- (void)setCustomBackgroundColor_:(id)color;
+- (void)setCustomBackgroundImage_:(id)imageName;
+- (void)setCustomActiveIconColor_:(id)color;
+- (void)setCustomActiveIndicator_:(id)imageName;
+
+@end
---
+++ Classes/Ti/TiUITabGroup+Extend.m
@@ -0,0 +1,46 @@
+/**
+ * Module developed by Napp
+ * Author Mads Møller
+ * www.napp.dk
+ *
+ * by ryugoo
+ */
+
+#import "TiUITabGroup+Extend.h"
+#import "TiUtils.h"
+
+@implementation TiUITabGroup (TiUITabGroup_Extend)
+
+- (void)setCustomBackgroundColor_:(id)color
+{
+    if([color isKindOfClass:[UIColor class]]) {
+        [[UITabBar appearance] setTintColor:color];
+    } else {
+        TiColor *tintColor = [TiUtils colorValue:color];
+        [[UITabBar appearance] setTintColor:[tintColor _color]];
+    }
+}
+
+- (void)setCustomBackgroundImage_:(id)imageName
+{
+    UIImage *backImage = [TiUtils loadBackgroundImage:imageName forProxy:self.proxy];
+    [[UITabBar appearance] setBackgroundImage:backImage];
+}
+
+- (void)setCustomActiveIconColor_:(id)color
+{
+    if([color isKindOfClass:[UIColor class]]) {
+        [[UITabBar appearance] setSelectedImageTintColor:color];
+    } else {
+        TiColor *activeIconColor = [TiUtils colorValue:color];
+        [[UITabBar appearance] setSelectedImageTintColor:[activeIconColor _color]];
+    }
+}
+
+- (void)setCustomActiveIndicator_:(id)imageName
+{
+    UIImage *activeImage = [TiUtils loadBackgroundImage:imageName forProxy:self.proxy];
+    [[UITabBar appearance] setSelectionIndicatorImage:activeImage];
+}
+
+@end
