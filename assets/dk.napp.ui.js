5a863a7c6f974f94c731eb9ce45ee3bc4fb5ca66
Terry Martin <terryzmartin@gmail.com>
2013-08-08 15:25:46 -0700

    Use embedded preloader file instead of commonJS for symbol instantiation

---
+++ assets/dk.napp.ui.js
@@ -0,0 +1,18 @@
+/*
+
+	Symbol Preloader. Use this to force Titanium to include class files. 
+	Only needed if you want to override delegate instance methods
+	See issue: https://github.com/viezel/NappUI/issues/5
+	
+*/
+Ti.UI.createWindow();
+Ti.UI.createTab();
+Ti.UI.createTabGroup();
+Ti.UI.createWindow();
+Ti.UI.iPhone.createNavigationGroup();
+Ti.UI.createTextField();
+Ti.UI.createWebView();
+Ti.UI.createSearchBar();
+Ti.UI.createScrollableView();
+Ti.UI.createPicker();
+Ti.UI.iOS.createToolbar();
